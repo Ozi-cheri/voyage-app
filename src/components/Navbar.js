66405fb9ac9f399b61a034a29e61243964d85be5
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-03-11 12:54:30 +0100

    Add favicon to public folder, add logo to assets folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png"; // Adjust the path as needed
 
-const NavigationBar = () => {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">Voyage</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
+function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">
+                <img 
+                    src={logo} 
+                    alt="Voyage Logo" 
+                    width="50" // Adjust size as needed
+                    height="50"
+                    className="d-inline-block align-top"
+                />
+                Voyage
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
 
-export default NavigationBar;
+export default Navigation;
