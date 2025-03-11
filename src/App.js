aa1333357786c1a535b5409d7ca0a48f88d56fd5
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-03-11 22:07:50 +0100

    Styled App with Google Fonts and added custom CSS for typographyadd app.module.css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import NavigationBar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./App.module.css"; // Import CSS Module
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}> {/* Apply CSS Module */}
       <NavigationBar />
       <h1>Welcome to Voyage</h1>
     </div>
