2682f96660b51f658e4572202dca4e985c2ac21f
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-03-11 19:35:40 +0100

    Add font awesome, update index.html and navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,26 +5,27 @@ import logo from "../assets/logo.png"; // Adjust the path as needed
 function Navigation() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">
+            <Navbar.Brand>
                 <img 
                     src={logo} 
                     alt="Voyage Logo" 
                     width="50" // Adjust size as needed
-                    height="50"
-                    className="d-inline-block align-top"
-                />
-                Voyage
+                    height="50" />
+                    
+                
+                
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
-                    <Nav.Link href="/contact">Contact</Nav.Link>
+                    <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
+                    <Nav.Link><i className="fa-solid fa-sign-in-alt"></i>Sign in</Nav.Link>
+                    <Nav.Link><i className="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
+                    <Nav.Link><i className="fa-solid fa-envelope"></i>Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
     );
-}
+};
 
 export default Navigation;
