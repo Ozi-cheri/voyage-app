a8de0986a3f2499d454a3b86828ac5fddaf9e6c0
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-03-12 20:40:34 +0100

    Implement structured routes for Home, Sign In, Sign Up, and Contact

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from "react";
 import NavigationBar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./App.module.css"; // Import CSS Module
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className={styles.container}> {/* Apply CSS Module */}
-      <NavigationBar />
-      <h1>Welcome to Voyage</h1>
-    </div>
+    <Router>  {/* Now Router is actually used */}
+      <div className={styles.container}> {/* Apply CSS Module */}
+        <NavigationBar />
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home page</h1>} />
+            <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+            <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+            <Route exact path="/contact" render={() => <h1>Contact</h1>} />
+            <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png"; // Adjust the path as needed
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 function Navigation() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar  className={styles.navbar}bg="light" expand="lg">
+            <Container>
+        
             <Navbar.Brand>
+            
                 <img 
                     src={logo} 
                     alt="Voyage Logo" 
@@ -18,12 +23,28 @@ function Navigation() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
-                    <Nav.Link><i className="fa-solid fa-sign-in-alt"></i>Sign in</Nav.Link>
-                    <Nav.Link><i className="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
-                    <Nav.Link><i className="fa-solid fa-envelope"></i>Contact</Nav.Link>
+                     {/* Home Link */}
+                     <Nav.Link as={NavLink} to="/" exact activeClassName={styles.active}>
+                        <i className="fa-solid fa-house"></i> Home
+                    </Nav.Link>
+
+                    {/* Sign In Link */}
+                    <Nav.Link as={NavLink} to="/signin" activeClassName={styles.active}>
+                        <i className="fa-solid fa-sign-in-alt"></i> Sign In
+                    </Nav.Link>
+
+                    {/* Sign Up Link */}
+                    <Nav.Link as={NavLink} to="/signup" activeClassName={styles.active}>
+                        <i className="fa-solid fa-user-plus"></i> Sign Up
+                    </Nav.Link>
+
+                    {/* Contact Link */}
+                    <Nav.Link as={NavLink} to="/contact" activeClassName={styles.active}>
+                        <i className="fa-solid fa-envelope"></i> Contact
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 };
